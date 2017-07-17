13f881793a397bfe53010353f017eb14b9fd15c6
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-18 06:16:19 +1000

    Styling prev and next buttons

--- src/public/partials/sharp-slideshow-public-display.php
+++ src/public/partials/sharp-slideshow-public-display.php
@@ -26,8 +26,8 @@
 			</a>
 		<?php endforeach; ?>
 
-		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		<a class="prev no-select" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next no-select" onclick="plusSlides(1)">&#10095;</a>
 	</div>
 	<div style="text-align:center">
 		<span class="dot" onclick="currentSlide(1)"></span> 
