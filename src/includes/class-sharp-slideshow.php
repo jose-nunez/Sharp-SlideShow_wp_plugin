603f6e0fc0565b9bd2d0cb92d7c24e4c82741a61
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-18 04:14:55 +1000

    Removed some comments

--- src/includes/class-sharp-slideshow.php
+++ src/includes/class-sharp-slideshow.php
@@ -94,28 +94,9 @@ class Sharp_Slideshow {
 	 * @access   private
 	 */
 	private function load_dependencies() {
-
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sharp-slideshow-loader.php';
-
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-sharp-slideshow-i18n.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-sharp-slideshow-admin.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-sharp-slideshow-public.php';
 
 		$this->loader = new Sharp_Slideshow_Loader();
--- src/public/class-sharp-slideshow-public.php
+++ src/public/class-sharp-slideshow-public.php
@@ -60,21 +60,7 @@ class Sharp_Slideshow_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Sharp_Slideshow_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Sharp_Slideshow_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/sharp-slideshow-public.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -83,21 +69,7 @@ class Sharp_Slideshow_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Sharp_Slideshow_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Sharp_Slideshow_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/sharp-slideshow-public.js', array( 'jquery' ), $this->version, false );
-
 	}
 
 
