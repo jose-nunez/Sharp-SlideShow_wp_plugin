b387d83fe33a9587b4cb14463b1521c30d368f2f
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-18 03:20:55 +1000

    Added shortcode support

--- src/includes/class-sharp-slideshow-loader.php
+++ src/includes/class-sharp-slideshow-loader.php
@@ -82,6 +82,24 @@ class Sharp_Slideshow_Loader {
 	}
 
 	/**
+	 * Add a new filter to the collection to be registered with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @param    string               $hook             The name of the WordPress filter that is being registered.
+	 * @param    object               $component        A reference to the instance of the object on which the filter is defined.
+	 * @param    string               $callback         The name of the function definition on the $component.
+	 * @param    int                  $priority         Optional. he priority at which the function should be fired. Default is 10.
+	 * @param    int                  $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1
+	 */
+	public function add_shortcode( $hook, $component, $callback) {
+		$this->shortcodes[] = array(
+			'hook'			=> $hook,
+			'component'		=> $component,
+			'callback'		=> $callback
+		);
+	}
+
+	/**
 	 * A utility function that is used to register the actions and hooks into a single
 	 * collection.
 	 *
@@ -122,6 +140,10 @@ class Sharp_Slideshow_Loader {
 
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+		}
+
+		foreach ( $this->shortcodes as $hook ) {
+			add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
 		}
 
 	}
