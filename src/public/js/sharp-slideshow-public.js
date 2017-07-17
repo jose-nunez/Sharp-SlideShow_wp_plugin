7c36ee6c593f136b82fc29b38766626d8f471590
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-18 04:06:54 +1000

    Slideshow test

--- src/public/js/sharp-slideshow-public.js
+++ src/public/js/sharp-slideshow-public.js
@@ -37,6 +37,34 @@
 	 
 })( jQuery );
 
+// console.log('alguna otra wea estoy haciendo');
 
 
-console.log('alguna otra wea estoy haciendo');
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1} 
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none"; 
+  	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block"; 
+	dots[slideIndex-1].className += " active";
+}
--- src/public/partials/sharp-slideshow-public-display.php
+++ src/public/partials/sharp-slideshow-public-display.php
@@ -13,10 +13,33 @@
  */
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div id="sharp-slideshow">
+	<div class="slideshow-container">
+		<div class="mySlides fade">
+			<div class="numbertext">1 / 3</div>
+			<img src="<?php echo plugin_dir_url( __FILE__ ) ?>../img/img1.jpg" style="width:100%">
+			<div class="text">Caption Text</div>
+		</div>
 
-<?php 
+		<div class="mySlides fade">
+			<div class="numbertext">2 / 3</div>
+			<img src="<?php echo plugin_dir_url( __FILE__ ) ?>../img/img2.jpg" style="width:100%">
+			<div class="text">Caption Two</div>
+		</div>
 
-	echo '<span id="holandaquetalca">HOLANDA QUE TALCA con PHP</span>';
+		<div class="mySlides fade">
+			<div class="numbertext">3 / 3</div>
+			<img src="<?php echo plugin_dir_url( __FILE__ ) ?>../img/img3.jpg" style="width:100%">
+			<div class="text">Caption Three</div>
+		</div>
 
- ?>
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+	</div>
+	<br />
+	<div style="text-align:center">
+		<span class="dot" onclick="currentSlide(1)"></span> 
+		<span class="dot" onclick="currentSlide(2)"></span> 
+		<span class="dot" onclick="currentSlide(3)"></span> 
+	</div>
+</div>
