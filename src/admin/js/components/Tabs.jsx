04434072aae44d1693412983626bdc1d0bac3d53
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-24 02:16:43 +1000

    Converted Tabs into React Component

--- src/admin/js/components/Tabs.jsx
+++ src/admin/js/components/Tabs.jsx
@@ -4,12 +4,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
 import RaisedButton from 'material-ui/RaisedButton';
 
-
 import PostsList from 'postsList';
-
-
-// import Divider from 'material-ui/Divider';
-// import Toggle from 'material-ui/Toggle';
 
 const styles = {
 	headline: {
@@ -24,35 +19,43 @@ function handleActive(tab) {
 	// alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
 }
 
-const TabsExampleSimple = () => (
-	<Tabs>
-		<Tab label="Item One" >
-			<div>
-				<h2 style={styles.headline}>Tab One</h2>
-				<PostsList />
-			</div>
-		</Tab>
-		<Tab label="Item Two" >
-			<div>
-				<h2 style={styles.headline}>Tab Two</h2>
-				<p>
-					This is another example tab.
-				</p>
-			</div>
-		</Tab>
-		<Tab
-			label="onActive"
-			data-route="/home"
-			onActive={handleActive}
-		>
-			<div>
-				<h2 style={styles.headline}>Tab Three</h2>
-				<p>
-					This is a third example tab.
-				</p>
-			</div>
-		</Tab>
-	</Tabs>
-);
+export default class TabsExampleSimple extends React.Component{
+	constructor(props) {
+		super(props);Object.keys(this).forEach(index=>{if(React.Component[index]==undefined && this[index] instanceof Function){this[index] = this[index].bind(this);}});
+	}
 
-export default TabsExampleSimple;
+	componentDidMount(){
+	}
+
+	render(){
+		return (
+			<Tabs>
+				<Tab label="Item One" >
+					<div>
+						<h2 style={styles.headline}>Tab One</h2>
+						<PostsList />
+					</div>
+				</Tab>
+				<Tab label="Item Two" >
+					<div>
+						<h2 style={styles.headline}>Tab Two</h2>
+						<p>
+						</p>
+					</div>
+				</Tab>
+				<Tab
+					label="onActive"
+					data-route="/home"
+					onActive={handleActive}
+				>
+					<div>
+						<h2 style={styles.headline}>Tab Three</h2>
+						<p>
+							This is a third example tab.
+						</p>
+					</div>
+				</Tab>
+			</Tabs>
+		);	
+	}
+}
